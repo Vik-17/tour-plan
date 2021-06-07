e738bb8cc90f0c11a7ba22ca2d20fb36419456cf
Vik-17 <zhakevichv@ya.ru>
2021-06-07 23:57:19 +0300

    create: added form validation and mask for phone

--- js/main.js
+++ js/main.js
@@ -52,4 +52,27 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please enter a name",
+          minlength: "Name must be at least 2 letters long",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Phone is required",
+        },
+      },
+    });
+  });
+
+  // Маска валидации
+  $('input[name="phone"]').mask("+7(000) 000 0000");
 });
